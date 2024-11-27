1066c11a570d5f27199738db8059cbfd93512919
Hinglerio Carlos <84482831+candez23@users.noreply.github.com>
2024-11-26 22:43:13 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const formulario = document.getElementById("form-contacto");
 // Botón para abrir/cerrar el menú
 const menuToggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
+const btnArriba = document.getElementById('btn-arriba');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        btnArriba.style.display = 'block';
+    } else {
+        btnArriba.style.display = 'none';
+    }
+});
+
+btnArriba.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
 
 menuToggle.addEventListener("click", () => {
     menu.classList.toggle("menu-open");
